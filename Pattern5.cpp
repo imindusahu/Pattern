175461a060a99f68143a98cb0f6272d7eea0fe49
Indu Sahu <142209457+imindusahu@users.noreply.github.com>
2024-09-04 18:23:50 +0530

    initial commit

---
+++ Pattern5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            if (col<=row)
+                cout << "*";
+                else
+                cout<<" ";
+        }
+        for (int col = 1; col < n; col++)
+        {
+            if (col >= (n-1)-row)
+                cout << "*";
+                else
+                cout<<" ";
+        }
+        cout << endl;
+    }
+}
+// *       *
+// **     **
+// ***   ***
+// **** ****
+// *********
