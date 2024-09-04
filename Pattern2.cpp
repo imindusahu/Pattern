395ad8f6c82854b833dedb316a4cab5930fbbe8c
Indu Sahu <142209457+imindusahu@users.noreply.github.com>
2024-09-04 18:23:16 +0530

    print inverted left pyramid

---
+++ Pattern2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    
+    for(int row=0;row<n;row++){
+        for(int col=0;col<n;col++){
+            if(col<=(n-1)-row)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+// *****
+// ****
+// ***
+// **
+// *
